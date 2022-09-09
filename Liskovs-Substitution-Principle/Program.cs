982448c916a69107827f659e79071130695adac2
iremakalp1@hotmail.com <iremakalp1@hotmail.com>
2022-09-09 20:15:38 +0300

    Liskovs Substitution Principle notes added

--- Liskovs-Substitution-Principle/Program.cs
+++ Liskovs-Substitution-Principle/Program.cs
@@ -1,2 +1,123 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Liskovs_Substitution_Principle
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //  Liskovs Substitution Principle
+            // yerine gecebilme prensibi
+            // alt siniflar ust siniflarinin miras aldigi butun ozellikleri kullanabilmeli
+            // alt siniflarda olusturulan nesneler ust siniflar ile yer degistirdiginde;
+            // ayni davranisi gostermeli ve herhangi bir kullanilmayan ozellik olmamali
+
+            Printer printer = new Printer();
+            printer.Print("value");
+
+            Console.WriteLine("************************");
+
+            PrinterNew printerNew = new PrinterNew();
+            printerNew.Print("value");
+        }
+    }
+
+    // kotu kod
+    // bu sekilde yazildiginde scan metodu her printer icin gecerli olmayabilir
+    // 
+    public abstract class BasePrinter
+    {
+        public abstract void Print(string value);
+        public abstract void Scan(string value);
+    }
+    public class HpPrinter : BasePrinter
+    {
+        public override void Print(string value)
+        {
+            // .....
+            Console.WriteLine("Hp Printer print ");
+        }
+        public override void Scan(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class CanonPrinter : BasePrinter
+    {
+        public override void Print(string value)
+        {
+            // ....
+            Console.WriteLine("Canon Printer print ");
+        }
+        public override void Scan(string value)
+        {
+            //...
+            Console.WriteLine("Canon Printer scan ");
+        }
+    }
+    public class Printer
+    {
+        readonly BasePrinter _canonPrinter = new CanonPrinter();
+        readonly BasePrinter _hpPrinter = new HpPrinter();
+
+        public void Print(string value)
+        {
+            // canon her ikisi icinde calisir
+            _canonPrinter.Print(value);
+            _canonPrinter.Scan(value);
+
+            // hp sadece print icin calisir
+            // scan metodu bos oldugu icin hata verir
+            _hpPrinter.Print(value);
+            _hpPrinter.Scan(value);
+        }
+    }
+
+    // dogru kod
+    // baseprinter icerisinde sadece printer metodu olmali
+    // scan emtodu ayri yazilmali
+    // canon ve hp printer ise ihtiyacı olan siniftan kalitilmali ya da implemente edilmeli
+
+    public abstract class BasePrinterNew
+    {
+        public abstract void Print(string value);
+    }
+    public interface IScan
+    {
+        void Scan(string value);
+    }
+
+    public class CanonPrinterNew : BasePrinterNew, IScan
+    {
+        public override void Print(string value)
+        {
+            Console.WriteLine("Canon Printer print ");
+        }
+        public void Scan(string value)
+        {
+            Console.WriteLine("Canon Printer scan ");
+        }
+    }
+    public class HpPrinterNew : BasePrinterNew
+    {
+        public override void Print(string value)
+        {
+            Console.WriteLine("Hp Printer print ");
+        }
+    }
+    public class PrinterNew
+    {
+        readonly BasePrinterNew _canonPrinter = new CanonPrinterNew();
+        readonly CanonPrinterNew _canonScaner = new CanonPrinterNew();
+        readonly BasePrinterNew _hpPrinter = new HpPrinterNew();
+
+        public void Print(string value)
+        {
+            _canonPrinter.Print(value);
+            _canonScaner.Scan(value);
+
+            _hpPrinter.Print(value);
+
+
+        }
+    }
+}
+
