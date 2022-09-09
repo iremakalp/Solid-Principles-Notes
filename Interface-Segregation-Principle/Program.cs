8b4e19770e799afe4a7b4a68a6c011092de09934
iremakalp1@hotmail.com <iremakalp1@hotmail.com>
2022-09-09 20:27:45 +0300

    Interface Segregation Principle

--- Interface-Segregation-Principle/Program.cs
+++ Interface-Segregation-Principle/Program.cs
@@ -1,2 +1,110 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Interface_Segregation_Principle
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Interface Segregation Principle
+            // arayuzlerin ayrilmasi prensibi
+            // sorumluluklarin hepsi bir arayuze toplamak yerine
+            // ozellestirilmis arayuzler olusturmaliyiz
+
+
+        }
+    }
+
+    // kotu kod
+
+    public interface IBaseApi
+    {
+        void Put(int id);
+        void Post(int id);
+        void Get(int id);
+        void Delete(int id);
+    }
+
+    public class News : IBaseApi
+    {
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Post(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Put(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Video:IBaseApi
+    {
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Post(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Put(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    // sadece get yapacagimiz bir durum olursa IBaseApi isimizi gormez
+    // bu durumlarin olmamasi icin Get metodu ayri interface olarak yazilir
+
+    // dogru kod
+
+    public interface IGet
+    {
+        void Get(int id);
+    }
+    public interface IBaseApiNew:IGet
+    {
+        void Put(int id);
+        void Post(int id);
+        void Delete(int id);
+    }
+    public class VideoNew : IBaseApiNew
+    {
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Post(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Put(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Notification : IGet
+    {
+        public void Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    // boylelikle notification sadece get metodunu icerirken
+    // video sinifi hepsini icerir
+
+}
